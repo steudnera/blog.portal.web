1ece02827151c366579c52d373cc742e1a42ad36
Philip <wowcxy2008@126.com>
2018-12-03 19:50:52 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,9 +14,8 @@ module.exports = {
   extends: 'standard',
   // add your custom rules here
   rules: {
-    // allow async-await
-    'generator-star-spacing': 'off',
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "indent": ["error", 4],
+    "generator-star-spacing": "off",
+    "no-debugger": process.env.NODE_ENV === ' production' ? 'error' : 'off'
   }
 }
