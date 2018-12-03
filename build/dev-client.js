2be5885fdbacd9f59af2b3f0817c597c8d853c5f
Philip <wowcxy2008@126.com>
2018-12-03 20:10:55 +0800

    Create dev-client.js

---
+++ build/dev-client.js
@@ -0,0 +1,13 @@
+/**
+ * 开发客户端
+ * @author Philip
+ */
+'use strict'
+require('eventsource-polyfill');
+var hotClient = require('webpack-hot-middleware/client?noInfo=true&reload=true');
+
+hotClient.subscribe(function (event) {
+  if (event.action === 'reload') {
+    window.location.reload();
+  }
+});
